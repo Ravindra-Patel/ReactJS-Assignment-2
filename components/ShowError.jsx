32f1c7d096a943c843fde742d95bec15728c7e81
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-01 10:24:30 +0530

    Added ShowError component to display the error messages

---
+++ components/ShowError.jsx
@@ -0,0 +1,12 @@
+const ShowError = () => {
+    return (
+      <div class="flex my-2 max-w-2xl p-3 bg-brightRedLight border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <p class="font-normal w-full m-5 text-white dark:text-gray-400">
+          <b>Internal Error Occurred!</b>
+        </p>
+      </div>
+    );
+  };
+  
+  export default ShowError;
+  
