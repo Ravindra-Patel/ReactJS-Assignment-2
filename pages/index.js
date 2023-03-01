850e020790a1587354c81539206d64273e97394d
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-01 10:33:16 +0530

    Modified the Homepage to finalize the flow of the application

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,56 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Dropdown from "../components/Dropdown";
+import { useEffect, useState } from "react";
+import { useCallback } from "react";
+import Models from "../components/Models";
 
-export default function Home() {
+export default function Home({ carsData }) {
+  let [tempModel, setTempModel] = useState(" ");
+  let [model, setModel] = useState("None");
+  let [cars, setCars] = useState([]);
+  let [showmodels, setShowModels] = useState(false);
+
+  let handleModelSelection = (e) => {
+    setShowModels(false);
+    setTempModel(e.target.value);
+  };
+
+  let fetcModels = () => {
+    setShowModels(true);
+    setModel(tempModel);
+  };
+
+  const setCarsFunction = useCallback(() => {
+    setCars(carsData);
+  }, [carsData]);
+
+  useEffect(() => {
+    setCarsFunction();
+  }, [setCarsFunction]);
+
   return (
-    <div className={styles.container}>
-      
+    <div className="min-h-[81vh] md:min-h-[78vh]">
+      <Dropdown
+        handleModelSelection={handleModelSelection}
+        fetchModels={fetcModels}
+        cars={cars}
+      />
+      {showmodels && <Models model={model} showmodels={showmodels} />}
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  let response = await fetch(
+    "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json"
+  );
+
+  console.log(response);
+  let data = await response.json();
+  let carsData = await data.Results;
+
+  return {
+    props: {
+      carsData,
+    },
+  };
 }
