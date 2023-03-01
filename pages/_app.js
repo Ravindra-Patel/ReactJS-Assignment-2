24216914ac2d782e8995c905cd7a34bb545053bd
Ravindra Patel <ravindra.patel@bluepineapple.io>
2023-03-01 10:31:34 +0530

    Added Application layout in the _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "../styles/globals.css";
+import HeadMeta from "../components/HeadMeta";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <HeadMeta />
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
